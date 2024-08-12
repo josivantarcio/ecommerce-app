a2084757765bf2c3d1531e892c38d04fe9ae2ed7
Josevan Oliveira <josivantarcio@msn.com>
2024-08-12 13:31:54 -0300

    modificado o server.js, criacao do config/database.json

--- api/server.js
+++ api/server.js
@@ -13,16 +13,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compression());
 app.use(cors());
 
-// Conexão ao banco de dados MongoDB (ajuste a URL de conexão conforme necessário)
-mongoose.connect('mongodb://localhost:27017/ecommerce');
+// Desativa o cabeçalho 'X-Powered-By'
+app.disable('x-powered-by');
+
+// Conexão ao banco de dados MongoDB
+mongoose.connect('mongodb://localhost:27017/ecommerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Rotas
 app.get('/', (req, res) => {
-    res.send('API do E-commerce - @josevan');
+  res.send('API do E-commerce - @josevan');
+});
+
+// Configuração de pastas estáticas (opcional, ajuste conforme necessário)
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Tratamento de erros
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  if (err.status !== 404) console.warn('Error: ', err.message, new Date());
+  res.json({ errors: { message: err.message, status: err.status } });
 });
 
 // Iniciando o servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
