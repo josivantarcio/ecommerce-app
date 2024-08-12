5fa8ae6764a7b0120613d93c1c5bc0b3393c0cb6
Josevan Oliveira <josivantarcio@msn.com>
2024-08-12 14:07:13 -0300

    Salvar mudanças antes de reverter

--- api/server.js
+++ api/server.js
@@ -1,50 +1,55 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
-// Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(compression());
+// Definir ambiente
+const isProduction = process.env.NODE_ENV === "production";
+const PORT = process.env.PORT || 5000;
+
+// Conectar ao banco de dados
+const dbs = require("./config/database");
+const dbURI = isProduction ? dbs.dbProduction : dbs.dbTest;
+mongoose.connect(dbURI);
+
+// Definir view engine, se necessário
+app.set("view engine", "ejs");
+
+// Middlewares
+if (!isProduction) app.use(morgan("dev"));
 app.use(cors());
-
-// Desativa o cabeçalho 'X-Powered-By'
 app.disable('x-powered-by');
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Conexão ao banco de dados MongoDB
-mongoose.connect('mongodb://localhost:27017/ecommerce', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Servir arquivos estáticos
+app.use("/public", express.static(path.join(__dirname, "/public")));
+app.use("/public/images", express.static(path.join(__dirname, "/public/images")));
 
 // Rotas
-app.get('/', (req, res) => {
-  res.send('API do E-commerce - @josevan');
-});
-
-// Configuração de pastas estáticas (opcional, ajuste conforme necessário)
-app.use('/public', express.static(path.join(__dirname, 'public')));
+require("./models");
+app.use("/", require("./routes"));
 
 // Tratamento de erros
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  if (err.status !== 404) console.warn('Error: ', err.message, new Date());
-  res.json({ errors: { message: err.message, status: err.status } });
+    res.status(err.status || 500);
+    if (err.status !== 404) console.warn("Error: ", err.message, new Date());
+    res.json({ erros: { message: err.message, status: err.status } });
 });
 
-// Iniciando o servidor
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+// Iniciar o servidor
+app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
